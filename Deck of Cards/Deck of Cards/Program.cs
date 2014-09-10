a2272e4d8682dd4eeaf3db4d83126e186e6d4e35
SeedPaths-User <lessdismissive@email.com>
2014-09-10 16:49:20 -0600

    Makes cards, decks, shuffles and deals

---
+++ Deck of Cards/Deck of Cards/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Deck_of_Cards
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //make a deck
+            Deck myDeck = new Deck();
+            for (int i = 0; i < myDeck.Contents.Count; i++)
+            {
+                Console.WriteLine(myDeck.Contents[i].Rank + " of " + myDeck.Contents[i].Suit);
+            }
+            Console.WriteLine("\n");
+            myDeck.Shuffle();
+            for (int i = 0; i < myDeck.Contents.Count; i++)
+            {
+                Console.WriteLine(myDeck.Contents[i].Rank + " of " + myDeck.Contents[i].Suit);
+            }
+
+            Console.WriteLine("\n");
+
+            List<Card> myHand = myDeck.Deal(5);
+            for (int i = 0; i < myHand.Count; i++)
+            {
+                Console.WriteLine(myHand[i].Rank + " of " + myHand[i].Suit);
+            }
+            
+
+            //keep the console open
+            Console.ReadKey();
+        }
+    }
+}
